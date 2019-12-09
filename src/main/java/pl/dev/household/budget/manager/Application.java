d5f4a5588ce100b6d6ee97e13e599754a4cbb5c4
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2019-12-09 23:32:11 +0100

    INIT commit of springboot app

---
+++ src/main/java/pl/dev/household/budget/manager/Application.java
@@ -0,0 +1,29 @@
+package pl.dev.household.budget.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+@EnableAutoConfiguration(exclude = {
+        DataSourceAutoConfiguration.class,
+})
+public class Application extends SpringBootServletInitializer {
+
+    private static Class<Application> applicationClass = Application.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+}
