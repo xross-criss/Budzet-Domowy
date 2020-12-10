df4a4af948dceaf04a1fd280b8881dbe81b1b30e
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2020-12-11 00:39:04 +0100

    Wallet services added

---
+++ src/main/java/pl/dev/household/budget/manager/controllers/WalletController.java
@@ -0,0 +1,44 @@
+package pl.dev.household.budget.manager.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.dev.household.budget.manager.domain.WalletDTO;
+import pl.dev.household.budget.manager.security.util.Security;
+import pl.dev.household.budget.manager.services.WalletService;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/wallet")
+public class WalletController {
+
+    private WalletService walletService;
+
+    public WalletController(WalletService walletService) {
+        this.walletService = walletService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<WalletDTO>> getWallets() throws Exception {
+        return ResponseEntity.ok(walletService.getWalletsInHousehold(Security.currentUser().getHousehold().getId()));
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<WalletDTO> addWallet(@RequestBody WalletDTO walletDTO) {
+        return ResponseEntity.ok(walletService.saveWallet(walletDTO));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public void updateWallet(@RequestBody WalletDTO walletDTO) throws Exception {
+        walletService.updateWallet(Security.currentUser().getId(), walletDTO);
+    }
+
+    @DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteWallet(@RequestParam(name = "id") Integer walletId) throws Exception {
+        walletService.deleteWallet(walletId);
+    }
+
+}
--- src/main/java/pl/dev/household/budget/manager/services/WalletService.java
+++ src/main/java/pl/dev/household/budget/manager/services/WalletService.java
@@ -3,9 +3,15 @@ package pl.dev.household.budget.manager.services;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import pl.dev.household.budget.manager.dao.User;
 import pl.dev.household.budget.manager.dao.Wallet;
+import pl.dev.household.budget.manager.dao.repository.UserRepository;
 import pl.dev.household.budget.manager.dao.repository.WalletRepository;
 import pl.dev.household.budget.manager.domain.WalletDTO;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -13,10 +19,12 @@ public class WalletService {
 
     private ModelMapper modelMapper;
     private WalletRepository walletRepository;
+    private UserRepository userRepository;
 
-    public WalletService(ModelMapper modelMapper, WalletRepository walletRepository) {
+    public WalletService(ModelMapper modelMapper, WalletRepository walletRepository, UserRepository userRepository) {
         this.modelMapper = modelMapper;
         this.walletRepository = walletRepository;
+        this.userRepository = userRepository;
     }
 
     public WalletDTO getWalletDTOById(Integer walletId) {
@@ -35,4 +43,35 @@ public class WalletService {
         walletRepository.deleteById(walletId);
     }
 
+    public List<WalletDTO> getWalletsInHousehold(Integer householdId) throws Exception {
+        List<User> usersInHousehold = userRepository.findAllByHousehold_Id(householdId).orElse(Collections.emptyList());
+
+        if (usersInHousehold.isEmpty()) {
+            throw new Exception("Household not found!");
+        }
+
+        List<WalletDTO> walletsInHousehold = new ArrayList<>();
+
+        usersInHousehold.forEach(user -> {
+            Wallet walletTmp = walletRepository.findByUserId(user.getId()).orElse(null);
+            if (walletTmp != null) {
+                walletsInHousehold.add(modelMapper.map(walletTmp, WalletDTO.class));
+            }
+        });
+
+        return walletsInHousehold;
+    }
+
+    public void updateWallet(Integer userId, WalletDTO walletDTO) throws Exception {
+        Wallet walletTmp = walletRepository.findById(walletDTO.getId()).orElse(null);
+
+        if (!walletTmp.getUser().getId().equals(userId)) {
+            throw new Exception("Wallet doesnt belong to User!");
+        }
+
+        Wallet wallet = modelMapper.map(walletDTO, Wallet.class);
+        wallet.setUser(walletTmp.getUser());
+
+        walletRepository.save(wallet);
+    }
 }
